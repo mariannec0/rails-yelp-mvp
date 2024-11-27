6de26d0c3cf3ae80aef6b8f761ebbaeffe5e795f
Marianne Cooper <mariannecooper@hotmail.co.uk>
2024-11-27 14:17:43 +0000

    finished yelp review without styling and refactoring

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# 1. Clean the database 🗑️
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+# 2. Create the instances 🏗️
+puts "Creating restaurants..."
+Restaurant.create!(name: "Chinese Kitchen", address: "7 Boundary St, London E2 7JE", phone_number: "07729472843", category: "chinese")
+Restaurant.create!(name: "GBK", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "07729472843", category: "belgian")
+Restaurant.create!(name: "Jolly", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "07729472843", category: "french")
+Restaurant.create!(name: "Ramen Kulture", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "07729472843", category: "japanese")
+Restaurant.create!(name: "Pizza Hut", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "07729472843", category: "italian")
+
+
+# 3. Display a message 🎉
+puts "Finished! Created #{Restaurant.count} restaurants."
