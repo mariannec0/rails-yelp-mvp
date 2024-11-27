a28d0de623f7a5432699fd9dfb35f8d6642e6cad
Marianne Cooper <mariannecooper@hotmail.co.uk>
2024-11-27 14:43:01 +0000

    added small styling and refactored

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,18 @@
 class ReviewsController < ApplicationController
-  before_action :set_restaurant, only: %i[new create]
+  before_action :set_restaurant, only: %i[create]
 
-  def new
-    # @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new
-  end
+  # def new
+  #   # @restaurant = Restaurant.find(params[:restaurant_id])
+  #   @review = Review.new
+  # end
 
   def create
-    # @restaurant = Restaurant.find(params[:restaurant_id])
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = @restaurant.reviews.build(review_params)
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new, status: :unprocessable_entity
+      render 'restaurants/show', status: :unprocessable_entity
     end
   end
 
